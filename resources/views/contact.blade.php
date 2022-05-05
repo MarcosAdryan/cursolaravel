7a297a95f69563f70a002afb82d9cb87a4536f32
MarcosAdryan <adryan.logo@hotmail.com>
2022-05-05 09:37:36 -0300

    primeiro commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Contact page</h1>
+<a href="/">Voltar para home</a>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Marcos";
+    $idade = '21';
+
+    $arr = [1,2,3,4,5];
+
+    $nomes = ['Matheus', 'Maria', 'Diego'];
+
+    return view('welcome',
+     [  
+        'nome' => $nome,
+        'idade' => $idade,
+        'profissao' => 'programador',
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
