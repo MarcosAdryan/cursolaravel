bb315282fcfaf0d127d734648b39df8728968839
MarcosAdryan <adryan.logo@hotmail.com>
2022-05-10 12:44:17 -0300

    até a aula 18 do curso de laravel

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -519,6 +519,7 @@ class ComposerStaticInitc243d46252d44a232b3fdcd1260c41d5
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Event' => __DIR__ . '/../..' . '/app/Models/Event.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
